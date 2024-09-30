c505e690bf50e1e0ce3f51d1096f5321dbabe345
Hyhello <673089899@qq.com>
2024-09-30 15:14:49 +0800

    fix: update package.json

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,10 +3,9 @@ import { build } from 'esbuild';
 // 构建
 build({
     entryPoints: ['src/init.ts'],
-    outfile: 'lib/index.mjs',
+    outfile: 'lib/index.cjs',
     bundle: true,
     minify: true,
-    format: 'esm',
     target: 'node14',
     platform: 'node'
     // external: ['giget', 'prompts']
